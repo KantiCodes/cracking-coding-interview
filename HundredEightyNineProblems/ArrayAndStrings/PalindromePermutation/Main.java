445e024be9d6cb6582ce10e6fb1bb6658afc1344
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-12-23 01:50:41 +0100

    1.4 Add Java optimized integer as bitset solution + fix previous cases for non letter characters

--- HundredEightyNineProblems/ArrayAndStrings/PalindromePermutation/Main.java
+++ HundredEightyNineProblems/ArrayAndStrings/PalindromePermutation/Main.java
@@ -17,7 +17,7 @@ public class Main {
         new Pair("a", true),
         new Pair("", true),
         new Pair("aaabb", true),
-        new Pair("aabbb", true),
+        new Pair("aab! bb", true),
         new Pair("aabbcc", true),
         new Pair("daabbcc", true),
         new Pair("daabbccdde", false),
@@ -30,35 +30,46 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println();
         Main main = new Main();
         for (Pair theCase: cases){
             boolean myResultHash = main.HashMapSolution(theCase.word);
             boolean myResultBit = main.bitArrayXorSolution(theCase.word);
+            boolean myResultBitSetOptimized = main.IntAsBitArraySolution(theCase.word);
             boolean expectedResult = theCase.result;
             if (myResultHash != expectedResult) {
                 System.out.println(String.format("The hash case: %s, failed, expected value: %b", theCase.word, expectedResult));
             }
             if (myResultBit != expectedResult) {
-                System.out.println(String.format("The bit case: %s, failed, expected value: %b", theCase.word, expectedResult));
+                System.out.println(String.format("The bit unoptimized case: %s, failed, expected value: %b", theCase.word, expectedResult));
             }
 
+            if (myResultBitSetOptimized != expectedResult) {
+                System.out.println(String.format("The bit optimized case: %s, failed, expected value: %b", theCase.word, expectedResult));
+            }
+
+
         }
+
     }
 
     public boolean HashMapSolution(String word){
         if (word.length() == 0 || word.length() == 1) {
             return true;
         }
-        HashMap<Character,Integer> myMap = new HashMap<>();
+        HashMap<Integer,Integer> myMap = new HashMap<>();
 
         for (char c: word.toCharArray()) {
-            int val = myMap.getOrDefault(c, 0);
-            myMap.put(c, val+1);
+            int c_int = intToChar(c);
+            if (c_int == -1) continue;
+            int val = myMap.getOrDefault(c_int, 0);
+            myMap.put(c_int, val+1);
         }
+        
 
         boolean uneven = false;
 
-        for (char c: myMap.keySet()) {
+        for (Integer c: myMap.keySet()) {
             if (myMap.get(c) %2 != 0) {
                 if (uneven) return false;
                 uneven = true;
@@ -71,7 +82,8 @@ public class Main {
     public boolean bitArrayXorSolution(String word){
         BitSet bitArr = new BitSet();
         for (char c: word.toCharArray()) {
-            int index = c - 65;
+            int index = intToChar(c);
+            if (index == -1) continue;
             boolean result = bitArr.get(index) ^ true;
             if (result) bitArr.set(index);
             else bitArr.clear(index);
@@ -85,5 +97,59 @@ public class Main {
         }
         return true;
     }
+    public boolean IntAsBitArraySolution(String word) {
+        int bitSet = 0;
+        for (char c: word.toCharArray()) {
+            int cInt = intToChar(c);
+            if (cInt != -1) {
+                bitSet = flipBitSet(bitSet, cInt);
+            }
+        }
+        // System.out.println(bitSet);
+        if (bitSet == 0) return true;
+        if (hasOneBitFlipped(bitSet)) return true;
+
+        return false;
+
+
+    }
+
+    public int intToChar(char c) {
+
+        int c_int = c;
+        int a = 'a';
+        int z = 'z';
+
+        if (a <= c_int && z >= c_int) {
+            return c_int - a;
+        }
+
+        return -1;
+    }
+
+    public int flipBitSet(int bitSet, int index) {
+
+        int value = 1 << index;
+
+        if ((value & bitSet) == 0) {
+            bitSet = bitSet | value;
+        }
+        else { 
+            bitSet = bitSet ^ value;
+        }
+        // System.out.println(1);
+        // System.out.println(bitSet);
+        // System.out.println(2);
+
+
+        return bitSet;
+    }
+
+    public boolean hasOneBitFlipped(int value) {
+
+        int result = value & (value - 1);
+        return result == 0;
+    }
+
 
 }
