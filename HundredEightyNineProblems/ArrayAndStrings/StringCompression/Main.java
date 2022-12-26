e472ebc187549e560b587fb5d8ba3121d122ecb5
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-12-26 23:19:25 +0100

    1.6: Added Java solution

---
+++ HundredEightyNineProblems/ArrayAndStrings/StringCompression/Main.java
@@ -0,0 +1,55 @@
+package HundredEightyNineProblems.ArrayAndStrings.StringCompression;
+
+public class Main {
+    public static class Pair{
+        public String word;
+        public String result;
+
+        public Pair(String word, String result) {
+            this.word = word;
+            this.result = result;
+        }
+
+        public static Pair[] testCases = {
+            new Pair("abc", "abc"),
+            new Pair("aaabc", "aaabc"),
+            new Pair("aabbcc", "aabbcc"),
+            new Pair("aabbbccdeeee", "a2b3c2d1e4")
+
+        };
+        public static void main(String[] args) {
+            Main main = new Main();
+            for (Pair testCase: testCases) {
+                String result = main.Solution(testCase.word);
+                if (!result.equals(testCase.result)) {
+                    System.out.println(String.format("Failed for %s, got: %s, expected: %s", testCase.word, result, testCase.result));
+                }
+            }
+        }
+    }
+
+
+    public String Solution(String word) {
+        int count = 1;
+        char lastChar = word.charAt(0);
+
+        StringBuilder s = new StringBuilder(2*word.length());
+        s.append(lastChar);
+
+        for (char c: word.substring(1, word.length()).toCharArray()) {
+            if (lastChar == c) {
+                count ++;
+            }
+            
+            else {
+                s.append(count);
+                s.append(c);
+                lastChar = c;
+                count = 1;
+            }
+        }
+        s.append(count);
+        if (s.length() >= word.length()) return word;
+        return s.toString();
+    }
+}
